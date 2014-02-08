d18a03e3fe10f03947a05f5fd7a14a11dc0b6607
PhilLeech <phillip.leech@actionfigurelabs.com>
2014-02-08 14:59:47 -0600

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ SampleApp::Application.routes.draw do
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
+
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   root 'static_pages#home'
 
   #match '/', to: 'static_pages#home', via: 'get'
@@ -11,9 +16,10 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
-  resources :users
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
